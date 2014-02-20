b29a2e232648736cf5141367cb68756b2676a935
Nick Rioux <nickr500@gmail.com>
2014-02-20 09:11:10 -0500

    Source Language definition

---
+++ SimpleCompiler/SourceLang.fs
@@ -0,0 +1,31 @@
+﻿module SourceLang =
+    type Id = int
+
+    type Type =
+        | Bool
+        | Product of Type * Type
+        | Function of Type * Type
+
+    type Expression =
+        | Variable of Id
+        | True
+        | False
+        | Pair of Expression * Expression
+        | Application of Expression * Expression
+        | Lambda of Id * Type * Expression
+        | Projection of int * Expression
+
+    let rec isValue e =
+        match e with
+        | True -> true
+        | False -> true
+        | Pair(e1, e2) -> (isValue e1) && (isValue e2)
+        | Lambda(_, _, _) -> true
+        | _ -> false
+
+
+
+
+
+
+
