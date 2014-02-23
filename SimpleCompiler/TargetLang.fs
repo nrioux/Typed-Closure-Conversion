89c20ab95e0c94abc6b36b1c678d4bb1ba039520
Nick Rioux <nickr500@gmail.com>
2014-02-23 15:50:43 -0500

    Major Target Language changes

--- SimpleCompiler/TargetLang.fs
+++ SimpleCompiler/TargetLang.fs
@@ -1,21 +1,21 @@
 ﻿module TargetLang
-    type Id = int
+    type Id = string
 
     type Type =
         | TypeVariable of Id
         | Bool
-        | Product of Type * Type
-        | Function of Type * Type
+        | Product of list<Type>
+        | Function of list<Id> * list<Type> * Type
         | Existential of Id * Type
 
     type Expression =
         | Variable of Id
         | True
         | False
-        | Pair of Expression * Expression
+        | Tuple of list<Expression>
         | If of Expression * Expression * Expression
-        | Application of Expression * Expression
-        | Lambda of Id * Type * Expression
+        | Application of Expression * list<Type> * list<Expression>
+        | Lambda of list<Id> * Map<Id, Type> * Expression
         | Projection of int * Expression
         (*pack   <t,     e> as       Ea.   t>*)
         | Pack of Type * Expression * Id * Type
@@ -26,8 +26,29 @@
         match e with
         | True -> true
         | False -> true
-        | Pair(e1, e2) -> (isValue e1) && (isValue e2)
+        | Tuple(items) -> List.forall isValue items
         | Lambda(_, _, _) -> true
         | Pack(_, e, _, _) -> isValue(e)
         | _ -> false
 
+    let rec map f e =
+        match e with
+        | Tuple(items) -> Tuple (List.map f items)
+        | If(e1, e2, e3) -> If(f e1, f e2, f e3)
+        | Application(e1, typeArgs, args) ->
+            Application(f e1, typeArgs, List.map f args)
+        | Lambda(id, t, body) -> Lambda(id, t, f body)
+        | Projection(i, e1) -> Projection(i, f e1)
+        (*pack   <t,     e> as       Ea.   t>*)
+        | Pack(t1, e1, id, t2) -> Pack(t1, f e1, id, t2)
+        (*unpack   <a,   x> = e         in e*)
+        | Unpack(tid, id, e1, e2) -> Unpack(tid, id, f e1, f e2)
+        | _ -> e
+
+
+    // e[target/replacement]
+    let rec substitute mapping e =
+        match e with
+        | Variable id -> if Map.containsKey id mapping then
+                            Map.find id mapping else e
+        | _ -> map (substitute mapping) e
